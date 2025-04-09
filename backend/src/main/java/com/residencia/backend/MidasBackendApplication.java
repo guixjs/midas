c0b4d122a7b00f5269c1be8539e0e761283efca4
Guilherme Felix <f3lixguilherme@gmail.com>
2025-04-09 14:54:47 -0300

    first commit

---
+++ backend/src/main/java/com/residencia/backend/MidasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.residencia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidasBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/residencia/backend/MidasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.residencia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
