887d22d21f6d3895e2c0ba950533eaaf67760884
Guilherme Felix <f3lixguilherme@gmail.com>
2025-04-10 16:37:46 -0300

    fix: criacao de transacao passando o id_usuario através do token

--- backend/src/main/java/com/residencia/backend/modules/controllers/transacao/TransacaoController.java
+++ backend/src/main/java/com/residencia/backend/modules/controllers/transacao/TransacaoController.java
@@ -3,6 +3,7 @@ package com.residencia.backend.modules.controllers.transacao;
 
 import com.residencia.backend.modules.models.TransacaoEntity;
 import com.residencia.backend.modules.services.transacao.CriarTransacaoService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transaction")
@@ -19,8 +22,10 @@ public class TransacaoController {
   private CriarTransacaoService criarTransacaoService;
 
   @PostMapping("/new")
-  public ResponseEntity<Object> create(@Valid @RequestBody TransacaoEntity transacao){
+  public ResponseEntity<Object> create(@Valid @RequestBody TransacaoEntity transacao, HttpServletRequest request){
     try{
+      var idUser = request.getAttribute("id_usuario");
+      transacao.setId_usuario(UUID.fromString(idUser.toString()));
       var resultado = this.criarTransacaoService.execute(transacao);
       return ResponseEntity.ok().body(resultado);
 
---
+++ backend/src/main/java/com/residencia/backend/shareds/seguranca/SecurityFilter.java
@@ -0,0 +1,42 @@
+package com.residencia.backend.shareds.seguranca;
+
+import com.residencia.backend.shareds.providers.JWTProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private JWTProvider jwtProvider;
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    String header = request.getHeader("Authorization");
+    SecurityContextHolder.getContext().setAuthentication(null);
+    System.out.println(header);
+
+    if(header != null){
+      var subjectToken = this.jwtProvider.validarToken(header);
+      if(subjectToken.isEmpty()){
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return;
+      }
+      request.setAttribute("id_usuario",subjectToken);
+      UsernamePasswordAuthenticationToken auth =
+          new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+      SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    filterChain.doFilter(request,response);
+  }
+}
