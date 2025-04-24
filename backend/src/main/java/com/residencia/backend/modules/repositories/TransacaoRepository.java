0c236b42beffbeb75604fab417ea7e8e00da4df2
lauraaoliveira <lauraoliveira2304@gmail.com>
2025-04-24 10:00:01 -0300

    commit para salvar alteracoes filtro(provisorio)

--- backend/src/main/java/com/residencia/backend/modules/repositories/TransacaoRepository.java
+++ backend/src/main/java/com/residencia/backend/modules/repositories/TransacaoRepository.java
@@ -2,8 +2,8 @@ package com.residencia.backend.modules.repositories;
 
 import com.residencia.backend.modules.models.TransacaoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.UUID;
 
-public interface TransacaoRepository extends JpaRepository<TransacaoEntity, UUID> {
+public interface TransacaoRepository extends JpaRepository<TransacaoEntity, UUID>, JpaSpecificationExecutor<TransacaoEntity> {
 }
